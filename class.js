d8931569f0e67a23f0dac64c498d6efee6149356
Jordan Heidlage <jordanheidlage@jordans-air.lan>
2022-05-20 19:05:05 -0400

    added confirm length loop)
    git push

--- class.js
+++ class.js
@@ -5,3 +5,17 @@ var lowercase = [ A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z]
 var number = [0,1,2,3,4,5,6,7,8,9]
 var specialChar =[!,$,%,&,*]
 
+
+var confirmLength = "";
+var confirmUppercase;
+var confirmLowercase;
+var confirmNumber;
+var confirmSpecialchar;
+
+function generatePassword() {
+    var confirmLength = (prompt("How many characters would you like in your password between 8-128?"));
+}
+    while(confirmLength < 8 === confirmLength > 128) {
+        alert("Password length must be 8-128 characters, give it another shot");
+        var confirmLength = ("How many characters would you like in your password between 8-128?");
+    }
